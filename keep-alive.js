0640a7545fa74b4390439e9cad8ab5b2f2d4d844
JigneshMahadik <jigneshmahadik2898@gmail.com>
2024-08-20 11:56:22 +0530

    Initial commit

---
+++ keep-alive.js
@@ -0,0 +1,19 @@
+// keep-alive.js
+
+const https = require('https');
+
+// URL of your Render backend
+const url = 'https://your-backend-url.onrender.com';
+
+function sendRequest() {
+    https.get(url, (res) => {
+        console.log(`Pinged ${url}: Status Code ${res.statusCode}`);
+    }).on('error', (e) => {
+        console.error(`Error: ${e.message}`);
+    });
+}
+
+// Send a request every 5 minutes (300,000 milliseconds)
+setInterval(sendRequest, 300000);
+
+console.log(`Started keep-alive script. Pinging ${url} every 5 minutes.`);
